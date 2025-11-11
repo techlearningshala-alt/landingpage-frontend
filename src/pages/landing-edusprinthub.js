db39be823abf6b703fe423b6250cb7aead3779fd
MIPL <mipl@MIPLs-MacBook-Air.local>
2025-11-11 11:41:57 +0530

    fix

--- src/pages/landing-edusprinthub.js
+++ src/pages/landing-edusprinthub.js
@@ -2,22 +2,23 @@ import Banner from "@/components/Banner";
 import { getLandingPageData } from "@/services/LandingPageService";
 import React from "react";
 
-export async function getStaticProps() {
-  const domainSlug = "edusprinthub";
-  const landingPageData = await getLandingPageData(domainSlug).then(
-    (response) => response?.data.data[0]
-  );
-  return {
-    props: {
-      landingPageData: landingPageData,
-    },
-    revalidate: 60,
-  };
-}
-const LandingEdusprinthub = ({ landingPageData }) => {
-  console.log(landingPageData, "landingPageData");
+// export async function getStaticProps() {
+//   const domainSlug = "edusprinthub";
+//   const landingPageData = await getLandingPageData(domainSlug).then(
+//     (response) => response?.data.data[0]
+//   );
+//   return {
+//     props: {
+//       landingPageData: landingPageData,
+//     },
+//     revalidate: 60,
+//   };
+// }
+const LandingEdusprinthub = () => {
+  // console.log(landingPageData, "landingPageData");
   return (
     <div>
+      <p>edusprinthub landing page</p>
       <Banner />
     </div>
   );
--- src/pages/landing-studybettertoday.js
+++ src/pages/landing-studybettertoday.js
@@ -2,22 +2,23 @@ import Banner from "@/components/Banner";
 import { getLandingPageData } from "@/services/LandingPageService";
 import React from "react";
 
-export async function getStaticProps() {
-  const domainSlug = "studybettertoday";
-  const landingPageData = await getLandingPageData(domainSlug).then(
-    (response) => response?.data.data[0]
-  );
-  return {
-    props: {
-      landingPageData: landingPageData,
-    },
-    revalidate: 60,
-  };
-}
-const LandingStudyBetterToday = ({ landingPageData }) => {
-  console.log(landingPageData, "landingPageData");
+// export async function getStaticProps() {
+//   const domainSlug = "studybettertoday";
+//   const landingPageData = await getLandingPageData(domainSlug).then(
+//     (response) => response?.data.data[0]
+//   );
+//   return {
+//     props: {
+//       landingPageData: landingPageData,
+//     },
+//     revalidate: 60,
+//   };
+// }
+const LandingStudyBetterToday = () => {
+  // console.log(landingPageData, "landingPageData");
   return (
     <div>
+      <p>studybettertoday landing page</p>
       <Banner />
     </div>
   );
