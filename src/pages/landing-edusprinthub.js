1f63531599dbaf205a99697721212bed462a7845
MIPL <mipl@MIPLs-MacBook-Air.local>
2025-11-11 11:29:10 +0530

    fix

---
+++ src/pages/landing-edusprinthub.js
@@ -0,0 +1,26 @@
+import Banner from "@/components/Banner";
+import { getLandingPageData } from "@/services/LandingPageService";
+import React from "react";
+
+export async function getStaticProps() {
+  const domainSlug = "edusprinthub";
+  const landingPageData = await getLandingPageData(domainSlug).then(
+    (response) => response?.data.data[0]
+  );
+  return {
+    props: {
+      landingPageData: landingPageData,
+    },
+    revalidate: 60,
+  };
+}
+const LandingEdusprinthub = ({ landingPageData }) => {
+  console.log(landingPageData, "landingPageData");
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
+
+export default LandingEdusprinthub;
