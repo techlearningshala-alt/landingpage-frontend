0674c6497bf9a134047180759b747f0f08f05f0b
MIPL <mipl@MIPLs-MacBook-Air.local>
2025-11-11 12:10:58 +0530

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,24 @@
 
 import Banner from "@/components/Banner";
+import { getLandingPageData } from "@/services/LandingPageService";
 import React from "react";
 
+export async function getStaticProps() {
+  const domainSlug = "edusprinthub";
+  const landingPageData = await getLandingPageData(domainSlug).then(
+    (response) => response?.data?.data[0] || null
+  );
+  return {
+    props: {
+      landingPageData: landingPageData,
+    },
+    revalidate: 60,
+  };
+}
 
-
-const Home = () => {
+const Home = ({landingPageData}) => {
+  console.log(landingPageData,"mydatya");
+  
   return <div>
     <Banner/>
   </div>;
